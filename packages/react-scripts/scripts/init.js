1bfd15cf76d676c6ed7506a9360ee4795445fd92
Vjeran Hanžek <vjeran.hanzek@axilis.com>
2018-03-30 10:48:27 +0200

    Minor fixes

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -217,16 +217,6 @@ module.exports = function(
   console.log(chalk.cyan(`  ${displayedCommand} test`));
   console.log('    Starts the test runner.');
   console.log();
-  console.log(
-    chalk.cyan(`  ${displayedCommand} ${useYarn ? '' : 'run '}eject`)
-  );
-  console.log(
-    '    Removes this tool and copies build dependencies, configuration files'
-  );
-  console.log(
-    '    and scripts into the app directory. If you do this, you can’t go back!'
-  );
-  console.log();
   console.log('We suggest that you begin by typing:');
   console.log();
   console.log(chalk.cyan('  cd'), cdpath);
