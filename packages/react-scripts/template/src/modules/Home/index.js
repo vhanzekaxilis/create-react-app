6b15138fdfa674ed350a3fbe3aba51dc5ac90560
Vjeran Hanžek <vjeran.hanzek@axilis.com>
2018-04-04 12:02:36 +0200

    Added Home module styling

--- packages/react-scripts/template/src/modules/Home/index.js
+++ packages/react-scripts/template/src/modules/Home/index.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
+import logo from '../../images/logo.svg';
+import './Home.css';
+
 class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <div className="Home">
+        <header className="Home-header">
+          <img src={logo} className="Home-logo" alt="logo" />
+          <h1 className="Home-title">Welcome to React</h1>
+        </header>
+        <p className="Home-intro">
+          To get started, edit <code>src/module/Home/index.js</code> and save to
+          reload.
+        </p>
+      </div>
+    );
   }
 }
 
